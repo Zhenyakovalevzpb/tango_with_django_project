b5ad71a055ccc213f2e290554670a2a8f2ccdd21
Zhenyakovazpb <zhenyakovalev12@yandex.ru>
2020-04-13 16:33:29 +0300

    fixed Page model

--- rango/models.py
+++ rango/models.py
@@ -10,11 +10,9 @@ class Category(models.Model):
         return self.name
 
 class Page(models.Model):
-    category = models.ForeignKey(
-        'Category',
-        on_delete=models.CASCADE,
-    )
+    category = models.ForeignKey('Category', on_delete=models.CASCADE,)
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
-
+    def __str__(self):
+        return self.title
