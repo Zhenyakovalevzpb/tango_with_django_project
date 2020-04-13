ba174301b85b5d09898531dc7e471f641873276d
Zhenyakovazpb <zhenyakovalev12@yandex.ru>
2020-04-13 17:10:17 +0300

    added 2 fields to category

--- rango/models.py
+++ rango/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
-
     class Meta:
         verbose_name_plural = 'Categories'
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
