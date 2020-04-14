c59ab11ace0a74a7f430681eec952f3c87568584
Zhenyakovazpb <zhenyakovalev12@yandex.ru>
2020-04-14 14:53:06 +0300

    changed viev of index page

--- rango/views.py
+++ rango/views.py
@@ -4,14 +4,17 @@ from rango.models import Category, Page
 
 
 def index(request):
+    context_dict = {}
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
+    page_list = Page.objects.order_by('-views')[:5]
+
+    context_dict['pages'] = page_list
+    context_dict['categories'] = category_list
     return render(request, 'rango/index.html', context_dict)
 
 
 def show_category(request, category_name_slug):
     context_dict = {}
-
     try:
         category = Category.objects.get(slug=category_name_slug)
         pages = Page.objects.filter(category=category)
