b6253ba3d914a9d0be3ca343d126b8591f7f0235
Zhenyakovazpb <zhenyakovalev12@yandex.ru>
2020-04-14 16:31:55 +0300

    added url to add forms

--- rango/urls.py
+++ rango/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('add_category/', views.add_category, name='add_category'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.show_category, name='show_category'),
 ]
