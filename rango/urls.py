2c15d02d81dce28cb82f19c39f991f11a7a95bda
Zhenyakovazpb <zhenyakovalev12@yandex.ru>
2020-04-16 15:32:56 +0300

    added login functionality

--- rango/urls.py
+++ rango/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('add_category/', views.add_category, name='add_category'),
     path('category/<category_name_slug>/add_page/', views.add_page, name='add_page'),
     path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.show_category, name='show_category'),
 ]
--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login
+from django.urls import reverse
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from rango.models import Category, Page
 
@@ -69,6 +71,7 @@ def about(request):
     print(request.user)
     return render(request, 'rango/about.html', {})
 
+
 def register(request):
     registered = False
     if request.method == 'POST':
@@ -94,3 +97,22 @@ def register(request):
                   {'user_form': user_form,
                    'profile_form': profile_form,
                    'registered': registered})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                return HttpResponse("Your Rango account is disabled.")
+        else:
+            print("Invalid login details: {0}, {1}".format(username, password))
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request, 'rango/login.html', {})
+
