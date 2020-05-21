1098b06fb231082d6654fb2fc5b8c2a5ee78a810
Zhenyakovazpb <zhenyakovalev12@yandex.ru>
2020-05-21 13:57:43 +0300

    added func to count visits

--- rango/views.py
+++ rango/views.py
@@ -5,15 +5,15 @@ from django.urls import reverse
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from rango.models import Category, Page
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 def index(request):
-    context_dict = {}
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
-
-    context_dict['pages'] = page_list
-    context_dict['categories'] = category_list
-    return render(request, 'rango/index.html', context_dict)
+    context_dict = {'categories': category_list, 'pages': page_list}
+    response = render(request, 'rango/index.html', context_dict)
+    visitor_cookie_handler(request, response)
+    return response
 
 
 def show_category(request, category_name_slug):
@@ -67,6 +67,7 @@ def add_page(request, category_name_slug):
 
 
 def about(request):
+
     print(request.method)
     print(request.user)
     return render(request, 'rango/about.html', {})
@@ -123,4 +124,15 @@ def user_logout(request):
     logout(request)
     return HttpResponseRedirect(reverse('index'))
 
+def visitor_cookie_handler(request, response):
+    visits = int(request.COOKIES.get('visits','1'))
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        '%Y-%m-%d %H:%M:%S')
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        response.set_cookie('last_visit', str(datetime.now()))
+    else:
+        response.set_cookie('last_visit', last_visit_cookie)
+    response.set_cookie('visits', visits)
 
