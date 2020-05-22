e860a3cce908e3a047e3aba594e04d546d0ae326
Zhenyakovazpb <zhenyakovalev12@yandex.ru>
2020-05-22 12:30:23 +0300

    added functionality to change your password

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -3,9 +3,17 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url, include
+from registration.backends.simple.views import RegistrationView
+
+class MyRegistrationView(RegistrationView):
+    def get_success_url(self, user):
+        return '/rango/'
 
 urlpatterns = [
     path('rango/', include('rango.urls')),
     path('admin/', admin.site.urls),
+    url(r'^accounts/register/$',
+        MyRegistrationView.as_view(),
+           name='registration_register'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
