ad43c3a0f67f64c83962477e182ef2948a3c0ce1
Zhenyakovazpb <zhenyakovalev12@yandex.ru>
2020-05-22 11:31:37 +0300

    changed registration mechanism

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
+    'registration'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,9 @@ STATICFILES_DIRS = [STATIC_DIR, ]
 # Media files
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
+
+REGISTRATION_OPEN = True
+ACCOUNT_ACTIVATION_DAYS = 7
+REGISTRATION_AUTO_LOGIN = True
+LOGIN_REDIRECT_URL = '/rango/'
+LOGIN_URL = '/accounts/login/'
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -2,8 +2,10 @@ from django.urls import include,path
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url, include
 
 urlpatterns = [
     path('rango/', include('rango.urls')),
     path('admin/', admin.site.urls),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
     ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
